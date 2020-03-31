3938d8dc4671f815ef904c0ce1cbee26d3a96e94
Bakari Mustafa <contact@bakarimustafa.com>
2020-03-31 11:17:28 +1000

    Update meta.js

--- src/data/meta.js
+++ src/data/meta.js
@@ -10,12 +10,12 @@ const {
 } = process.env
 
 module.exports = {
-    title: META_TITLE || 'Queensland COVID-19 alert ',
-    url: META_URL || '',
-    description: META_DESC || 'An emergency information website.',
+    title: META_TITLE || 'Queensland COVID-19',
+    url: META_URL || 'https://queensland-covid-19.netlify.com/',
+    description: META_DESC || 'See daily reports on COVID-19 case numbers and the current situation in Queensland, Australia and Other States.',
     lang: META_LANG || 'en',
     primaryColor: META_COLOR || '#DB0000',
     email: META_EMAIL || undefined,
-    telephone: META_TELEPHONE || undefined,
+    telephone: META_TELEPHONE || '1800 020 080',
     dateFormat: 'dd LLLL yyyy'
 }
